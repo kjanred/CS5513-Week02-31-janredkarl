81c2ba54cf10519239214bcd01170702f4f307e0
kjanred <142561613+kjanred@users.noreply.github.com>
2023-08-30 01:57:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const http = require("http");
+const fs = require("fs").promises;
+
+const requestListener = function(req, res) {
+  console.log(req.url);
+  if (req.url === "/") {
+    fs.readFile( __dirname + "/home.html" )
+    .then (
+      contents => {
+        res.setHeader("Content-Type", "text/html; charset=UTF-8");
+        res.writeHead(200);
+        res.end(contents);
+      }
+    )
+  } else {
+    fs.readFile( __dirname + "/data.json")
+    .then( contents => {
+      res.setHeader("Content-Type", "application/json; charset=UTF-8");
+      res.writeHead(200);
+      res.end(contents);
+    }
+
+      
+    )
+    
+  }
+};
+
+const server = http.createServer(requestListener);
+
+const host ="0.0.0.0"
+const port ="8080"
+
+server.listen(
+ port,
+  host,
+  () => {
+  console.log("Server is Running!")
+  }
+);
